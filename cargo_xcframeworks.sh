a9d33bf173ea7207dabc909442f1baa00362057f
Stephan Diederich <stephan.diederich@gmail.com>
2023-03-03 19:09:05 +0100

    hack to make this work on macOS Ventura
    * drop macCatalyst builds for now, they failed
    * switch to nightly by default, because build errors
    * adapt to libautomerge_core rename

--- cargo_xcframeworks.sh
+++ cargo_xcframeworks.sh
@@ -7,7 +7,7 @@ rustup update
 
 echo "x86_64-apple-ios-macabi and aarch64-apple-ios-macabi require the nightly toolchain"
 rustup toolchain install nightly
-#rustup default nightly
+rustup default nightly
 
 # to allow for abi builds from the nightly toolchain for xargo...
 rustup component add rust-src
@@ -40,12 +40,12 @@ cargo build --target aarch64-apple-darwin --package automerge-c --release
 echo "▸ Build x86_64-apple-darwin"
 cargo build --target x86_64-apple-darwin --package automerge-c --release
 
-echo "▸ x86_64-apple-ios-macabi"
-xargo build -Zbuild-std --target x86_64-apple-ios-macabi --package automerge-c --release
+# echo "▸ x86_64-apple-ios-macabi"
+# xargo build -Zbuild-std --target x86_64-apple-ios-macabi --package automerge-c --release
 
-echo "▸ aarch64-apple-ios-macabi"
-#xargo build --target aarch64-apple-ios-macabi --package automerge-c --release
-xargo build -Zbuild-std --target aarch64-apple-ios-macabi --package automerge-c --release
+# echo "▸ aarch64-apple-ios-macabi"
+# #xargo build --target aarch64-apple-ios-macabi --package automerge-c --release
+# xargo build -Zbuild-std --target aarch64-apple-ios-macabi --package automerge-c --release
 
 # echo "▸ aarch64-apple-tvos"
 # xargo build -Zbuild-std --target aarch64-apple-tvos --package automerge-c --release
@@ -54,42 +54,44 @@ xargo build -Zbuild-std --target aarch64-apple-ios-macabi --package automerge-c 
 echo "▸ Lipo macOS"
 mkdir -p ./target/apple-darwin/release
 lipo -create  \
-    ./target/x86_64-apple-darwin/release/libautomerge.a \
-    ./target/aarch64-apple-darwin/release/libautomerge.a \
-    -output ./target/apple-darwin/release/libautomerge.a
+    ./target/x86_64-apple-darwin/release/libautomerge_core.a \
+    ./target/aarch64-apple-darwin/release/libautomerge_core.a \
+    -output ./target/apple-darwin/release/libautomerge_core.a
 
 echo "▸ Lipo simulator"
 mkdir -p ./target/apple-ios-simulator/release
 lipo -create  \
-    ./target/x86_64-apple-ios/release/libautomerge.a \
-    ./target/aarch64-apple-ios-sim/release/libautomerge.a \
-    -output ./target/apple-ios-simulator/release/libautomerge.a
+    ./target/x86_64-apple-ios/release/libautomerge_core.a \
+    ./target/aarch64-apple-ios-sim/release/libautomerge_core.a \
+    -output ./target/apple-ios-simulator/release/libautomerge_core.a
 
 echo "▸ Lipo ios-macabi"
 mkdir -p ./target/apple-ios-macabi/release
 lipo -create  \
-    ./target/aarch64-apple-ios-macabi/release/libautomerge.a \
-    ./target/x86_64-apple-ios-macabi/release/libautomerge.a \
-    -output ./target/apple-ios-macabi/release/libautomerge.a
+    ./target/aarch64-apple-ios-macabi/release/libautomerge_core.a \
+    ./target/x86_64-apple-ios-macabi/release/libautomerge_core.a \
+    -output ./target/apple-ios-macabi/release/libautomerge_core.a
 
 echo "#####################"
 rm -rf ./xcframework/AutomergeBackend.xcframework
 
 mkdir -p automerge-swift-backend/Headers
-cp automerge-c/automerge.h automerge-swift-backend/Headers
+cp ./automerge-c/build/include/automerge-c/automerge.h automerge-swift-backend/Headers
 cp automerge-swift-backend/module.modulemap automerge-swift-backend/Headers
 
 echo "▸ Create AutomergeRSBackend.xcframework"
   xcodebuild -create-xcframework \
-            -library ./target/apple-ios-simulator/release/libautomerge.a \
+            -library ./target/apple-ios-simulator/release/libautomerge_core.a \
             -headers ./automerge-swift-backend/Headers \
-            -library ./target/aarch64-apple-ios/release/libautomerge.a \
+            -library ./target/aarch64-apple-ios/release/libautomerge_core.a \
             -headers ./automerge-swift-backend/Headers \
-            -library ./target/apple-ios-macabi/release/libautomerge.a \
-            -headers ./automerge-swift-backend/Headers \
-            -library ./target/apple-darwin/release/libautomerge.a \
+            -library ./target/apple-darwin/release/libautomerge_core.a \
             -headers ./automerge-swift-backend/Headers \
             -output ./xcframework/AutomergeBackend.xcframework
+
+  # -library ./target/apple-ios-macabi/release/libautomerge_core.a \
+  # -headers ./automerge-swift-backend/Headers \
+
 
 echo "▸ Compress AutomergeRSBackend.xcframework"
 ditto -c -k --sequesterRsrc --keepParent ./xcframework/AutomergeBackend.xcframework ./automerge-swift-backend/AutomergeBackend.xcframework.zip
